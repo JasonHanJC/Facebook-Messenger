fc2f3133c038694b85cb0656eba790443fc0a19d
Juncheng Han <namrie1990@gmail.com>
2016-12-07 13:25:05 -0800

    Init

---
+++ Facebook Messenger/FriendListController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facebook Messenger
+//
+//  Created by Juncheng Han on 12/7/16.
+//  Copyright © 2016 Juncheng Han. All rights reserved.
+//
+
+import UIKit
+
+class FriendListController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
